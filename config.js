56d9ade1e0084d149f2fed4249209df45a597dc5
Jana150704 <Jana.Jungbluth@edu.fhdw.de>
2025-06-12 22:32:49 +0200

    config.js neu

---
+++ config.js
@@ -0,0 +1,32 @@
+// config.js: Speichert alle Konstanten und Konfigurationen des Spiels.
+
+export const BOARD_WIDTH = 700;
+export const BOARD_HEIGHT = 950;
+
+export const BIRD_DEFAULTS = {
+    WIDTH: 40,
+    HEIGHT: 30,
+    START_X: 50,
+    START_Y: BOARD_HEIGHT / 2,
+    JUMP_VELOCITY: -6,
+};
+
+export const PHYSICS = {
+    GRAVITY: 0.5,
+    PIPE_SPEED: -2,
+};
+
+export const PIPE_DEFAULTS = {
+    WIDTH: 50,
+    GAP: 200,
+    INTERVAL: 1500, // Zeit in ms zwischen dem Erscheinen neuer Rohre
+    VERTICAL_VARIATION: 100, // Maximale vertikale Abweichung zur letzten Lücke
+};
+
+// Spielzustände
+export const GAME_STATE = {
+    MENU: "menu",
+    PLAYING: "playing",
+    GAME_OVER: "gameOver",
+    PAUSED: "paused",
+};
